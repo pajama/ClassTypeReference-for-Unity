e7f18f7ac1f5cb22154dc19ac3a429e9351503ad
SolidAlloy <perepelartem@gmail.com>
2020-07-24 07:58:39 +0300

    Added refactored monoscript attribute for the future reference.

---
+++ Editor/MonoScriptPropertyDrawer.cs
@@ -0,0 +1,81 @@
+﻿/*****
+ * This is a simple PropertyDrawer for string variables to allow drag and drop
+ * of MonoScripts in the inspector of the Unity3d editor.
+ *****/
+
+namespace Editor
+{
+    using System.Collections.Generic;
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(MonoScriptAttribute), false)]
+    public class MonoScriptPropertyDrawer : PropertyDrawer
+    {
+        private static readonly Dictionary<string, MonoScript> ScriptCache;
+        private bool _viewString = false;
+
+        static MonoScriptPropertyDrawer()
+        {
+            ScriptCache = new Dictionary<string, MonoScript>();
+            var scripts = Resources.FindObjectsOfTypeAll<MonoScript>();
+
+            foreach (MonoScript script in scripts)
+            {
+                var type = script.GetClass();
+                if (type != null && !ScriptCache.ContainsKey(type.FullName))
+                    ScriptCache.Add(type.FullName, script);
+            }
+        }
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.String)
+            {
+                GUI.Label(position, "The MonoScript attribute can only be used on string variables");
+                return;
+            }
+
+            Rect prefixLabelRect = EditorGUI.PrefixLabel(position, label);
+            Rect labelRect = position;
+            labelRect.xMax = prefixLabelRect.xMin;
+            position = prefixLabelRect;
+            _viewString = GUI.Toggle(labelRect, _viewString, string.Empty, "label");
+            if (_viewString)
+            {
+                property.stringValue = EditorGUI.TextField(position, property.stringValue);
+                return;
+            }
+
+            MonoScript script = null;
+            string typeName = property.stringValue;
+            if (!string.IsNullOrEmpty(typeName))
+            {
+                ScriptCache.TryGetValue(typeName, out script);
+                if (script == null)
+                    GUI.color = Color.red;
+            }
+
+            script = (MonoScript) EditorGUI.ObjectField(position, script, typeof(MonoScript), false);
+            if (!GUI.changed)
+                return;
+
+            if (script == null)
+            {
+                property.stringValue = string.Empty;
+                return;
+            }
+
+            var type = script.GetClass();
+            var attr = (MonoScriptAttribute) attribute;
+            if (attr.ParentType != null && !attr.ParentType.IsAssignableFrom(type))
+            {
+                Debug.LogWarning("The script file " + script.name + " doesn't contain an assignable class");
+            }
+            else
+            {
+                property.stringValue = script.GetClass().Name;
+            }
+        }
+    }
+}
---
+++ Runtime/MonoScriptAttributeTrial.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public class MonoScriptAttributeTrial : MonoBehaviour
+{
+    [MonoScript(typeof(ExamplePrivateClass))] [SerializeField]
+    private string _classType;
+
+    [ValueDropdown("GetListOfMonoBehaviours", AppendNextDrawer = true)] [SerializeField]
+    private MonoBehaviour _someBehaviour;
+
+    private IEnumerable<MonoBehaviour> GetListOfMonoBehaviours()
+    {
+        return GameObject.FindObjectsOfType<MonoBehaviour>();
+    }
+
+    [Button]
+    public void PrintType()
+    {
+        Debug.Log($"{_classType}");
+    }
+
+    private class ExamplePrivateClass
+    {
+
+    }
+
+    private class ExamplePrivateClassImpl1 : ExamplePrivateClass
+    {
+    }
+
+    private class ExamplePrivateClassImpl2 : ExamplePrivateClass
+    {
+    }
+}
